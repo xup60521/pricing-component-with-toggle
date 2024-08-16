d941f7dfc1973dce46c2a989c4c29ebfa1d34099
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-16 21:03:02 +0800

    finish

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import gsap from "gsap";
+
+const switchBtn = document.getElementById("switch") as HTMLButtonElement;
+
+let isAnnually = true;
+
+switchBtn?.addEventListener("click", () => {
+    console.log("is triggered");
+    if (isAnnually) {
+        switchBtn.disabled = true;
+        changeToMonthlyPrice()
+        gsap.to("#switch  div", {
+            x: "1.5rem",
+            duration: 0.3,
+            ease: "power2.out",
+            onComplete: () => {
+                isAnnually = false;
+                switchBtn.disabled = false;
+            },
+        });
+    } else {
+        switchBtn.disabled = true;
+        changeToAnnuallyPrice()
+        gsap.to("#switch  div", {
+            x: "0",
+            duration: 0.3,
+            ease: "power2.out",
+            onComplete: () => {
+                isAnnually = true;
+                switchBtn.disabled = false;
+            },
+        });
+    }
+});
+
+function changeToMonthlyPrice() {
+    const basicPrice = document.getElementById("basic-price") as HTMLSpanElement
+    const professionalPrice = document.getElementById("professional-price") as HTMLSpanElement
+    const masterPrice = document.getElementById("master-price") as HTMLSpanElement
+    basicPrice.innerHTML = "19.99"
+    professionalPrice.innerHTML = "24.99"
+    masterPrice.innerHTML = "39.99"
+}
+
+function changeToAnnuallyPrice() {
+    const basicPrice = document.getElementById("basic-price") as HTMLSpanElement
+    const professionalPrice = document.getElementById("professional-price") as HTMLSpanElement
+    const masterPrice = document.getElementById("master-price") as HTMLSpanElement
+    basicPrice.innerHTML = "199.99"
+    professionalPrice.innerHTML = "249.99"
+    masterPrice.innerHTML = "399.99"
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Very_Light_Grayish_Blue: "hsl(240, 78%, 98%)",
+                c_Light_Grayish_Blue: "hsl(234, 14%, 74%)",
+                c_Grayish_Blue: "hsl(233, 13%, 49%)",
+                c_Dark_Grayish_Blue: "hsl(232, 13%, 33%)",
+                c_blue_1: "hsl(236, 72%, 79%)",
+                c_blue_2: "hsl(237, 63%, 64%)"
             },
             fontFamily: {
-
+                montserrat: ["Montserrat", "sans-serif"]
             }
         },
     },
